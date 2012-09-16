e2ec13ad50f181aab2859447c146edc63b5d5288
Idris <sld7700@gmail.com>
2012-09-17 02:06:33 +0600

    WebApp for 15 puzzle. With shuffle method.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "http://rubygems.org"
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'haml'
+gem 'fifteen_puzzle', :git => "git://github.com/sld/astar-15puzzle-solver.git" 
---
+++ app.rb
@@ -0,0 +1,36 @@
+require "bundler/setup"
+
+require 'sinatra'
+require 'sinatra/json'
+require 'matrix'
+require 'fifteen_puzzle'
+require 'json'
+
+
+class Array
+	def to_4_matrix
+		matrix_rows = []
+		row = []
+		self.each_with_index do |e, i| 
+			if ( (i + 1) % 4 == 0)
+				row << e
+				matrix_rows << row
+				row = []				
+			else
+				row << e
+			end
+		end
+		row_1, row_2, row_3, row_4 = matrix_rows
+		return Matrix[row_1, row_2, row_3, row_4]
+	end
+end
+
+get '/' do
+	haml :index
+end
+
+post '/shuffle.json' do
+	board = params[:board].map{|e| e.to_i}.to_4_matrix
+	game_matrix = FifteenPuzzle::GameMatrix.new board
+	json game_matrix.shuffle.matrix.to_a.flatten
+end	
