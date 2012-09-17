090c1cc096699b5019f51623f94b0edd9285b6c1
Idris <sld7700@gmail.com>
2012-09-17 08:26:02 +0600

    Added post solve.json. Some javascript in views.

--- app.rb
+++ app.rb
@@ -34,3 +34,12 @@ post '/shuffle.json' do
 	game_matrix = FifteenPuzzle::GameMatrix.new board
 	json game_matrix.shuffle.matrix.to_a.flatten
 end	
+
+post '/solve.json' do
+	board = params[:board].map{|e| e.to_i}.to_4_matrix
+	game_matrix = FifteenPuzzle::GameMatrix.new board
+	algorithm = FifteenPuzzle::AStarAlgorithm.new( game_matrix )
+	algorithm.run
+	solution_self_and_parents_arr = algorithm.solution.self_and_parents.map{|e| e.matrix.to_a.flatten}.reverse
+	json solution_self_and_parents_arr
+end
